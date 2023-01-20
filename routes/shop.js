4c2b2a02adb50896552b787152355d2e906db97a
ankurinfoniti <ankur.infoniti@gmail.com>
2023-01-20 19:11:50 +0530

    add more field to product page

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,6 @@ const shopController = require('../controllers/shop');
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
-
 router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.getCart);
 router.get('/checkout', shopController.getCheckout);
