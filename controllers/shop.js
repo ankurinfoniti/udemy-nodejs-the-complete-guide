0a89f70a9d5a1cabcc57f158a784951ab6243d56
ankurinfoniti <ankur.infoniti@gmail.com>
2023-01-24 16:32:45 +0530

    edit product

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+
   Product.findById(prodId, (product) => {
     res.render('shop/product-detail', {
       product: product,
@@ -41,6 +42,7 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+
   Product.findById(prodId, (product) => {
     Cart.addProduct(prodId, product.price);
   });
